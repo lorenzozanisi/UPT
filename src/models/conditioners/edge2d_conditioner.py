ff7c36c14232523db1090a749b1aed5081bd6a94
Zanisi, Lorenzo <pr5739@ukaea.uk>
2025-03-18 09:59:11 +0000

    using ContinuousSincosEmbed, which however doesnt seem to have 1d conditioning impmlemented

--- src/models/conditioners/edge2d_conditioner.py
+++ src/models/conditioners/edge2d_conditioner.py
@@ -1,7 +1,7 @@
 from kappamodules.init import init_xavier_uniform_zero_bias, init_truncnormal_zero_bias
 from kappamodules.layers import ContinuousSincosEmbed
-from torch import nn
-
+from torch import nn, zeros
+from utils.condition_embed import ContinuousConditionEmbed
 from models.base.single_model_base import SingleModelBase
 
 
@@ -12,16 +12,17 @@ class Edge2dConditioner(SingleModelBase):
         self.dim = dim
         self.cond_dim = cond_dim or dim * 4
         self.init_weights = init_weights
-        self.static_ctx["condition_dim"] = self.cond_dim
+        self.static_ctx["condition_dim"] = self.cond_dim 
 
         for cond_var in self.conditioning_vars:
             setattr(self,cond_var,ContinuousSincosEmbed(dim=dim, ndim=1))
-        self.conditioning_mlp = nn.Sequential(
-            nn.Linear(dim, dim * 4),
-            nn.GELU(),
-            nn.Linear(dim * 4, self.cond_dim),
-            nn.GELU(),
-        )
+            setattr(self,cond_var+"_mlp",nn.Sequential(
+                nn.Linear(dim, dim * 4),
+                nn.GELU(),
+                nn.Linear(dim * 4, self.cond_dim),
+                nn.GELU(),
+            ))    
+    
         # init
         self.reset_parameters()
 
@@ -33,16 +34,20 @@ class Edge2dConditioner(SingleModelBase):
         else:
             raise NotImplementedError
 
-    def forward(self, timestep, velocity):
+    def forward(self, conditioning):
         # checks + preprocess
-        assert timestep.numel() == len(timestep)
-        assert velocity.numel() == len(velocity)
-        timestep = timestep.flatten()
-        velocity = velocity.view(-1, 1).float()
-        # for rollout timestep is simply initialized as 0 -> repeat to batch dimension
-        if timestep.numel() == 1:
-            timestep = timestep.repeat(velocity.numel())
+        # assert timestep.numel() == len(timestep)
+        # assert velocity.numel() == len(velocity)
+        # timestep = timestep.flatten()
+        # velocity = velocity.view(-1, 1).float()
+        # # for rollout timestep is simply initialized as 0 -> repeat to batch dimension
+        # if timestep.numel() == 1:
+        #     timestep = timestep.repeat(velocity.numel())
         # embed
-        timestep_embed = self.timestep_mlp(self.timestep_embed[timestep])
-        velocity_embed = self.velocity_mlp(self.velocity_embed(velocity))
-        return timestep_embed + velocity_embed
+        embedded = zeros(self.cond_dim).to(self.device)
+        assert len(self.conditioning_vars) == len(conditioning)
+        for cond_var in self.conditioning_vars:
+            condition = getattr(self,cond_var)(conditioning[cond_var])
+            embedded += getattr(self,cond_var+"_mlp")(condition)
+
+        return embedded
